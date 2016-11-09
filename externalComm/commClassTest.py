8891d03cc486e470868cd8ef4f9faeb2d4199b31
athielk <alexthielk@gmail.com>
2016-11-09 01:44:25 -0800

    firebaseCom tests made v1

--- externalComm/commClassTest.py
+++ externalComm/commClassTest.py
@@ -1,4 +1,13 @@
-import flexmock
+import unittest
 from commClass import FirebaseComm
 
-flexmock(FirebaseComm)
+class TestFirebaseComm(unittest.TestCase):
+    def test_write(self):
+        commTest=FirebaseComm(True)
+        self.assertEqual(commTest.write('/test','this is a test'),{'1':'this is a test'})
+
+    def test_read(self):
+        commTest = FirebaseComm(True)
+        self.assertEqual(commTest.read('/test',None),{'1':'this is a test'})
+if __name__ == '__main__':
+    unittest.main()
