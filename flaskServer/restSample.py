e8cda81375e8411d36b98647257f5cfcd20545d0
Gustavo Cornejo <gcornejo85@gmail.com>
2016-11-10 13:09:06 -0800

    cleaned up upload endpoint code

--- flaskServer/restSample.py
+++ flaskServer/restSample.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for,request, redirect 
 from flask_restful import Resource, Api, reqparse
 from werkzeug import secure_filename
-
 import os
 
 app = Flask(__name__)
@@ -12,18 +11,16 @@ app.config['UPLOAD_FOLDER'] = 'static/img/'
 class uploadPhoto(Resource):
 	
 	def get(self):
-		return {'status': 'success'}
+		return {'server status': 'server is up and running'}
 		
 
 	def post(self):
 		self.reqparse = reqparse.RequestParser()
-		#self.reqparse.add_argument('picture', type=werkzeug.datastructures.FileStorage, location='files')
 		args = self.reqparse.parse_args()
 		f = request.files['photo']
 		filename = secure_filename(f.filename)
 		f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		return 'file uploaded successfully'
-
+		return {'upload status' : 'file upload sucessful'}
 
 
 api.add_resource(uploadPhoto, '/api/uploadImage')
