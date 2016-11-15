c07ef7e5b1d48a52807d0f979b1e8f1d2b5c00e9
Andrew Tran <andrew.tran345@gmail.com>
2016-11-14 19:31:21 -0800

    renamed files

---
+++ eventClass/aeroCubeSignal.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class AeroCubeSignal():
+
+    class ImageEventSignal(Enum):
+        IDENTIFY_AEROCUBES = 0x00010001
+        GET_AEROCUBE_POSE  = 0x00010002
+
+    class ResultEventSignal(Enum):
+        IMP_OPERATION_OK   = 0x00020001
