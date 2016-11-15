39f0c990c36f9d03bca67c65a556e4b5e84cd26a
Andrew Tran <andrew.tran345@gmail.com>
2016-11-14 19:05:46 -0800

    Init commit

---
+++ eventClass/aeroCubeEventTest.py
@@ -0,0 +1,12 @@
+from signal import Signal
+import unittest
+
+
+class SignalTest(unittest.TestCase):
+
+    def test_get_image_event_signal(self):
+        self.assertIsNotNone(Signal.ImageEventSignal.IDENTIFY_AEROCUBES)
+
+
+if __name__ == '__main__':
+    unittest.main()
