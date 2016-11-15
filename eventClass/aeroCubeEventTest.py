eab3dd5b2b716add88080ab5126642817ba0a390
Andrew Tran <andrew.tran345@gmail.com>
2016-11-14 19:31:21 -0800

    renamed files

--- eventClass/aeroCubeEventTest.py
+++ eventClass/aeroCubeEventTest.py
@@ -1,11 +1,11 @@
-from signal import Signal
+from aeroCubeSignal import AeroCubeSignal
 import unittest
 
 
 class SignalTest(unittest.TestCase):
 
     def test_get_image_event_signal(self):
-        self.assertIsNotNone(Signal.ImageEventSignal.IDENTIFY_AEROCUBES)
+        self.assertIsNotNone(AeroCubeSignal.ImageEventSignal.IDENTIFY_AEROCUBES)
 
 if __name__ == '__main__':
     unittest.main()
