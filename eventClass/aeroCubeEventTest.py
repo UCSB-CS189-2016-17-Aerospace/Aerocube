e7e37cae58cab174764adb8d898c3d8b7c9e5d41
andrewtran1995 <andrew.tran345@gmail.com>
2016-11-21 16:04:17 -0800

    Created is_valid_signal

--- eventClass/aeroCubeEventTest.py
+++ eventClass/aeroCubeEventTest.py
@@ -1,8 +1,16 @@
+from aeroCubeEvent import AeroCubeEvent, ImageEvent, ResultEvent
 from aeroCubeSignal import AeroCubeSignal
 import unittest
 
 
-class SignalTest(unittest.TestCase):
+class AeroCubeEventTest(unittest.TestCase):
+    def test_image_event_init(self):
+        event = ImageEvent(AeroCubeSignal.ImageEventSignal.IDENTIFY_AEROCUBES)
+        self.assertIsNotNone(event)
+        self.assertIsNotNone(event.signal)
+        self.assertIsNotNone(event.created_at)
+
+class AeroCubeSignalTest(unittest.TestCase):
 
     def test_get_image_event_signal(self):
         self.assertIsNotNone(AeroCubeSignal.ImageEventSignal.IDENTIFY_AEROCUBES)
