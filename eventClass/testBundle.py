729c81ec7f1c7f4094e245d4c0f5d30fc80fb2bc
Eric Swenson <elswenson@umail.ucsb.edu>
2016-11-22 10:52:06 -0800

    Switched to using setUpClass and tearDownClass methods, bundle methods now check key validity first, created structure for unit testing error message correctness

--- eventClass/testBundle.py
+++ eventClass/testBundle.py
@@ -1,80 +1,113 @@
 import unittest
-from bundle import Bundle
+from bundle import Bundle, BundleKeyError
 import pickle
 
 
 class TestBundle(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls._bundle = Bundle()
+        cls._messages = Bundle._ERROR_MESSAGES
+        # Keys
+        cls._valid_key = 'VALID_KEY'
+        cls._invalid_key_has_num = 'INVAL1D_K3Y'
+        cls._invalid_key_has_symbol = 'INV@lID_K3Y'
+        cls._invalid_key_has_lower = 'inVALID_KEY'
+        # Numbers
+        cls._valid_num = 42
+        cls._invalid_num = 'not a num'
+        # Strings
+        cls._valid_string = 'Valid string'
+        cls._invalid_string = 0
+        # Iterables
+        cls._valid_iterable = []
+        cls._valid_iterable_string = ''
+        cls._invalid_iterable = 0
+        # Raws
+        cls._valid_raw = pickle.dumps('valid raw')
+        cls._invalid_raw_string = 'invalid raw'
+        cls._invalid_raw_number = 0
+    
+    @classmethod
+    def tearDownClass(cls):
+        cls._bundle = None
+        # Reset iterable
+        cls._valid_iterable = []
 
     # Keys
 
     def test_valid_key(self):
-        self.assertEqual(Bundle.is_valid_key('VALID_KEY'), True)
+        self.assertEqual(self._bundle.is_valid_key(self._valid_key), True)
 
     def test_invalid_key_has_lowercase(self):
-        self.assertEqual(Bundle.is_valid_key('invalid_key'), False)
+        self.assertEqual(self._bundle.is_valid_key(self._invalid_key_has_lower), False)
 
     def test_invalid_key_has_number(self):
-        self.assertEqual(Bundle.is_valid_key('1NVAL1D_K3Y'), False)
+        self.assertEqual(self._bundle.is_valid_key(self._invalid_key_has_num), False)
 
     def test_invalid_key_has_symbol(self):
-        self.assertEqual(Bundle.is_valid_key('INV@LID_KEY'), False)
+        self.assertEqual(self._bundle.is_valid_key(self._invalid_key_has_symbol), False)
+
+    def test_bundle_access_valid_key_not_found(self):
+        self.assertRaises(BundleKeyError, self._bundle.strings, self._valid_key)
+
+    def test_bundle_access_invalid_key(self):
+        self.assertRaises(AttributeError, self._bundle.strings, self._invalid_key_has_lower)
 
     # Numbers
 
     def test_bundle_invalid_number_set(self):
-        bundle = Bundle()
-        key = 'INVALID_NUMBER'
-        invalid_num = ' '
         with self.assertRaises(AttributeError):
-            bundle.insert_number(key, invalid_num)
+            self._bundle.insert_number(self._valid_key, self._invalid_num)
 
     def test_bundle_valid_number(self):
-        bundle = Bundle()
-        key = 'VALID_NUMBER'
-        valid_num = 42
-        bundle.insert_number(key, valid_num)
-        self.assertEqual(bundle.numbers(key), valid_num)
+        self._bundle.insert_number(self._valid_key, self._valid_num)
+        self.assertEqual(self._bundle.numbers(self._valid_key), self._valid_num)
 
     # Strings
 
     def test_bundle_invalid_string_set(self):
-        bundle = Bundle()
-        key = 'INVALID_STRING'
-        invalid_string = 42
-        self.assertRaises(AttributeError, bundle.insert_string, key, invalid_string)
+        self.assertRaises(AttributeError, self._bundle.insert_string, self._valid_key, self._invalid_string)
 
     def test_bundle_valid_string(self):
-        bundle = Bundle()
-        key = 'VALID_STRING'
-        valid_string = 'This is a string'
-        bundle.insert_string(key, valid_string)
-        self.assertEqual(bundle.strings(key), valid_string)
+        self._bundle.insert_string(self._valid_key, self._valid_string)
+        self.assertEqual(self._bundle.strings(self._valid_key), self._valid_string)
 
     # Raws
 
-    def test_bundle_raw_not_string(self):
-        bundle = Bundle()
-        key = 'INVALID_RAW'
-        invalid_raw = ' not raw'
-        self.assertRaises(AttributeError, bundle.insert_raw, key, invalid_raw)
+    def test_bundle_invalid_raw_string(self):
+        self.assertRaises(AttributeError, self._bundle.insert_raw, self._valid_key, self._invalid_raw_string)
 
-    def test_bundle_raw_not_number(self):
-        bundle = Bundle()
-        key = 'INVALID_RAW'
-        invalid_raw = 0
-        self.assertRaises(AttributeError, bundle.insert_raw, key, invalid_raw)
+    def test_bundle_invalid_raw_number(self):
+        self.assertRaises(AttributeError, self._bundle.insert_raw, self._valid_key, self._invalid_raw_number)
 
-    def test_bundle_raw(self):
-        bundle = Bundle()
-        key = 'INVALID_RAW'
-        raw_value = pickle.dumps('asdf', pickle.HIGHEST_PROTOCOL)
-        bundle.insert_raw(key, raw_value)
-        bundled_val = bundle.raws(key=key)
-        self.assertEqual(bundled_val, raw_value)
+    def test_bundle_valid_raw(self):
+        self._bundle.insert_raw(self._valid_key, self._valid_raw)
+        bundled_val = self._bundle.raws(self._valid_key)
+        self.assertEqual(bundled_val, self._valid_raw)
 
     # Iterables
 
-    # TODO: Iterables Testing
+    def test_bundle_insert_iterable(self):
+        self._bundle.insert_iterable(self._valid_key, self._valid_iterable)
+        self.assertEqual(self._bundle.iterables(self._valid_key), self._valid_iterable)
+
+    def test_bundle_insert_non_iterable(self):
+        with self.assertRaises(AttributeError):
+            self._bundle.insert_iterable(self._valid_key, self._invalid_iterable)
+
+    def test_bundle_insert_string_counts_as_iterable(self):
+        self._bundle.insert_iterable(self._valid_key, self._valid_iterable)
+        self.assertEqual(self._bundle.iterables(self._valid_key), self._valid_iterable)
+
+    def test_bundle_update_iterable(self):
+        self._bundle.insert_iterable(self._valid_key, self._valid_iterable)
+        self.assertEqual(self._bundle.iterables(self._valid_key), self._valid_iterable)
+        # Test updated iterable
+        updated_iterable = self._valid_iterable
+        updated_iterable.append('item')
+        self._bundle.insert_iterable(self._valid_key, updated_iterable)
+        self.assertEqual(self._bundle.iterables(self._valid_key), self._valid_iterable)
 
 
 
