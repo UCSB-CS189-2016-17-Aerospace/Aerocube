cee68ba814a62e16517574493fdd992bd80c833c
andrewtran1995 <andrew.tran345@gmail.com>
2016-11-29 15:49:57 -0800

    Minor bug fixes and testing in controller.py

--- controller/tcpServer.py
+++ controller/tcpServer.py
@@ -4,29 +4,30 @@ from eventClass.aeroCubeEvent import AeroCubeEvent
 
 
 class TcpServer:
-    def __init__(self,ip,port,bufferSize):
+    def __init__(self, ip, port, bufferSize):
         self.TCP_IP = ip
         self.TCP_PORT = port
         self.BUFFER_SIZE = bufferSize
-        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.s.bind((self.TCP_IP,self.TCP_PORT))
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conn = None
+        self.addr = None
+        self.s.bind((self.TCP_IP, self.TCP_PORT))
 
     def accept_connection(self):
         self.s.listen(1)
-        global conn, addr
-        conn, addr = self.s.accept()
+        self.conn, self.addr = self.s.accept()
         print('TcpServer: Connection accepted')
 
     def send_response(self, response):
         encoded_response = dill.dumps(response)
         try:
-            conn.send(encoded_response)
+            self.conn.send(encoded_response)
             print('TcpServer: Response sent')
         except socket.error as e:
             print('Cant send response to client: %s' % e)
 
     def receive_data(self):
-        data = conn.recv(self.BUFFER_SIZE)
+        data = self.conn.recv(self.BUFFER_SIZE)
         message = dill.loads(data)
         if isinstance(message, AeroCubeEvent):
             print('TcpServer: Data Received')
@@ -35,6 +36,4 @@ class TcpServer:
             raise AttributeError('ERROR: Data must be a dilld Event')
 
     def close_connection(self):
-        conn.close()
-
-
+        self.conn.close()
