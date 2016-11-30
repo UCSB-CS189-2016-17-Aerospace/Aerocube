fe68f98a15f707e18893df5ddf11467a264deb31
andrewtran1995 <andrew.tran345@gmail.com>
2016-11-30 00:24:42 -0800

    Updated curlCommand to attempt to upload two images (one w/ marker, one w/out)

--- curlCommand.sh
+++ curlCommand.sh
@@ -1,1 +1,4 @@
+echo "Curling /home/ubuntu/GitHub/Aerocube/ImP/imageProcessing/test_files/jetson_test1.jpg"
 curl -F photo=@/home/ubuntu/GitHub/Aerocube/ImP/imageProcessing/test_files/jetson_test1.jpg https://localhost:3000/api/uploadImage -k
+echo "/home/ubuntu/GitHub/Aerocube/flaskServer/static/img/epicEarthMoon.png"
+curl -F photo=@/home/ubuntu/GitHub/Aerocube/flaskServer/static/img/epicEarthMoon.png https://localhost:3000/api/uploadImage -k
