d3ff6ec36f9b28cfe58b684ffb340dd6ccfd6639
andrewtran1995 <andrew.tran345@gmail.com>
2016-12-18 15:57:07 -0800

    Refactored strings to be stored in variables or accessed via settings file

--- flaskServer/settings.py
+++ flaskServer/settings.py
@@ -2,6 +2,19 @@ import os
 
 
 class FlaskServerSettings():
+    _static_img_rel_path = 'static/img/'
+    _test_files_rel_path = 'test_files'
+
     @staticmethod
     def get_flask_server_dir():
         return os.path.dirname(__file__)
+
+    @staticmethod
+    def get_static_img_dir():
+        return os.path.join(FlaskServerSettings.get_flask_server_dir(),
+                            FlaskServerSettings._static_img_rel_path)
+
+    @staticmethod
+    def get_test_files_dir():
+        return os.path.join(FlaskServerSettings.get_flask_server_dir(),
+                            FlaskServerSettings._test_files_rel_path)
