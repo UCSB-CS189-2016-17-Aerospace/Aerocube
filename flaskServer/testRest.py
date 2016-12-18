ee76f212c356e3dc2df42543ae3cded0fab3d9b2
andrewtran1995 <andrew.tran345@gmail.com>
2016-12-18 15:57:07 -0800

    Refactored strings to be stored in variables or accessed via settings file

--- flaskServer/testRest.py
+++ flaskServer/testRest.py
@@ -10,32 +10,36 @@ from .settings import FlaskServerSettings
 
 
 class RestTestCase(unittest.TestCase):
-    _sample_image_path = os.path.join(FlaskServerSettings.get_flask_server_dir(), 'static/img/sampleImage.png')
+    _test_img = 'ucsb_logo.jpg'
+    _static_img_dir = FlaskServerSettings.get_static_img_dir()
+    _test_img_path = os.path.join(FlaskServerSettings.get_test_files_dir(), _test_img)
+    _static_img_path = os.path.join(FlaskServerSettings.get_static_img_dir(), _test_img)
 
     def setUp(self):
         restEndpoint.app.config['TESTING'] = True
         self.app = restEndpoint.app.test_client()
 
     def tearDown(self):
-        subprocess.call(['rm', 'static/img/sampleImage.png'])
+        subprocess.call(['rm', self._static_img_path])
+        pass
 
     def test_successful_upload(self):
-        files = {'photo': open('sampleImage.png', 'rb')}
-        response = requests.post('http://127.0.0.1:5000/api/uploadImage', files=files)
+        files = {'photo': open(self._test_img_path, 'rb')}
+        response = requests.post('http://127.0.0.1:5005/api/uploadImage', files=files)
         self.assertEqual(response.status_code, 200)
 
     def test_file_existence(self):
-        files = {'photo': open('sampleImage.png', 'rb')}
-        ls_command = subprocess.check_output(['ls', 'static/img'])
-        self.assertFalse('sampleImage.png' in ls_command,
-                         msg='Precondition: {0} image should not exist at the API image upload endpoint.'.format('sampleImage.png'))
+        files = {'photo': open(self._test_img_path, 'rb')}
+        ls_command = subprocess.check_output(['ls', self._static_img_dir])
+        self.assertFalse(self._test_img in ls_command,
+                         msg='Precondition: {0} image should not exist at the API image upload endpoint.'.format(self._test_img))
         existence = False
-        requests.post('http://127.0.0.1:5000/api/uploadImage', files=files)
-        ls_command = subprocess.check_output(['ls', 'static/img'])
-        if 'sampleImage.png' in ls_command:
+        requests.post('http://127.0.0.1:5005/api/uploadImage', files=files)
+        ls_command = subprocess.check_output(['ls', self._static_img_dir])
+        if self._test_img in ls_command:
             existence = True
         self.assertTrue(existence,
-                        msg='Postcondition: {0} was not sucessfully created at the API image upload endpoint.'.format('sampleImage.png'))
+                        msg='Postcondition: {0} was not sucessfully created at the API image upload endpoint.'.format(self._test_img))
 
 
 if __name__ == '__main__':
