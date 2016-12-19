4c5f3ad5fb0278ec3ea2f4135d0362aca33ca1b0
athielk <alexthielk@gmail.com>
2016-12-18 17:16:30 -0800

    pip 8

--- externalComm/commClassTest.py
+++ externalComm/commClassTest.py
@@ -2,36 +2,42 @@ import unittest
 from commClass import FirebaseComm
 from externalComm import *
 import os.path
+
+
 class TestFirebaseComm(unittest.TestCase):
     commTest = FirebaseComm(True)
+
     def test_write(self):
-        self.commTest.write('test','2','this is a test2'),
-        self.assertEqual(self.commTest.read('test','2'),'this is a test2')
+        self.commTest.write('test', '2', 'this is a test2')
+        self.assertEqual(self.commTest.read('test', '2'), 'this is a test2')
         #testing delete
-        self.commTest.delete('test','2')
-        self.assertIsNone(self.commTest.read('test','2'))
+        self.commTest.delete('test', '2')
+        self.assertIsNone(self.commTest.read('test', '2'))
 
     def test_read(self):
-        self.assertEqual(self.commTest.read('test','1'),'this is a test')
+        self.assertEqual(self.commTest.read('test', '1'), 'this is a test')
 
     def test_imageStore(self):
-        self.commTest.imageStore('test','testimage.jpg')
+        self.commTest.imageStore('test', 'testimage.jpg')
 
     def test_imageDownload(self):
         self.commTest.imageDownload('test')
         self.assertTrue(os.path.isfile('test.jpg'))
 
-class externalComm(unittest.TestCase):
+
+class ExternalComm(unittest.TestCase):
 
     def test_process_store(self):
-        external_write(database= 'firebase',location='test',scanID='4',data='this is a process test2',testing=True)
-        self.assertEqual(external_read(database='firebase',location='test',scanID='4',testing=True),'this is a process test2')
+        external_write(database='firebase', location='test', scanID='4', data='this is a process test2', testing=True)
+        self.assertEqual(external_read(database='firebase', location='test', scanID='4', testing=True), 'this is a process test2')
         # testing process delete
-        external_delete(database='firebase',location='test',scanID='4',testing=True)
-        self.assertIsNone(external_read( database='firebase', location='test', scanID='4', testing=True))
+        external_delete(database='firebase', location='test', scanID='4', testing=True)
+        self.assertIsNone(external_read(database='firebase', location='test', scanID='4', testing=True))
+
 
     def test_process_read(self):
-        self.assertEqual(external_read( database='firebase', location='test', scanID='3', testing=True), 'this is a process test')
+        self.assertEqual(external_read(database='firebase', location='test', scanID='3', testing=True), 'this is a process test')
+
 
     def test_process_storeImage(self):
         external_store_img(database='firebase',scanID='processtest',srcImage='testimage.jpg',testing=True)
--- externalComm/externalComm.py
+++ externalComm/externalComm.py
@@ -1,31 +1,36 @@
 import json
 from commClass import FirebaseComm
-#TODO: data_json finalization
-def __selectDatabase__(database,testing):
-    comm=None
-    if(database=='firebase'):
-        comm=FirebaseComm(testing)
+##TODO: data_json finalization
+
+
+def __select_database__(database, testing):
+    if database == 'firebase':
+        return FirebaseComm(testing)
     else:
         raise ValueError('database not found!')
-    return comm
+
 
 def external_write(database, scanID, location=None, data=None, testing=False):
-    comm=__selectDatabase__(database=database,testing=testing)
+    comm = __select_database__(database=database, testing=testing)
     return comm.write(location=location, id=scanID, data=data)
 
+
 def external_store_img(database, scanID, srcImage=None, testing=False):
-    comm = __selectDatabase__(database=database,testing=testing)
-    return comm.imageStore( id=scanID, srcImage=srcImage)
+    comm = __select_database__(database=database, testing=testing)
+    return comm.imageStore(id=scanID, srcImage=srcImage)
+
 
 def external_read(database, scanID, location=None, testing=False):
-    comm = __selectDatabase__(database=database,testing=testing)
+    comm = __select_database__(database=database, testing=testing)
     return comm.read(location=location,id=scanID)
 
+
 def external_delete(database, scanID, location=None, testing=False):
-    comm = __selectDatabase__(database=database,testing=testing)
+    comm = __select_database__(database=database, testing=testing)
     return comm.delete(location=location,id=scanID)
 
+
 def external_download_image(database, scanID, testing=False):
-    comm = __selectDatabase__(database=database,testing=testing)
+    comm = __select_database__(database=database, testing=testing)
     return comm.imageDownload(scanID)
 
