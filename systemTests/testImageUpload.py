06e4cddf63c093daf0dd39543a9f65ef2f863149
andrewtran1995 <andrew.tran345@gmail.com>
2017-01-12 17:35:47 -0800

    Initialized integration test file for image upload main flow

---
+++ systemTests/testImageUpload.py
@@ -0,0 +1,7 @@
+import unittest
+from controller.controller import controller
+
+
+class TestImageUpload(unittest.testcase):
+    def run_controller_and_handler_and_upload_image(self):
+        pass
