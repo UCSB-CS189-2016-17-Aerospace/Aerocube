167ebd11ba9edb97a546a2dfcae846cb026b1040
Angel Ortega <an22ortega@gmail.com>
2017-01-17 04:38:29 -0800

    AO - adjusted logger to a shorter name for easier conventions, and I added some documentation

--- logger/tests/TestLogbook.py
+++ logger/tests/TestLogbook.py
@@ -5,22 +5,29 @@ from logbook import StreamHandler, Logger
 
 class TestLogbook(unittest.TestCase):
 
-  def setUp(self):
-    self._output_stream = StreamHandler(sys.stdout)
-    self._ctx = self._output_stream.push_application()
-    self._logger = Logger('TestLog')
+    def setUp(self):
+        """
+        Initialize Handler for arbitrary streams
+        with a parameter to log to the standard output.
+        Logger can be any name. The logger itself is a log channel
+        or a record dispatcher. In this case, the logger is set to
+        channel logs to the standard output
+        :return:
+        """
+        self._output_stream = StreamHandler(sys.stdout)
+        self._ctx = self._output_stream.push_application()
+        self._log = Logger('TestLog')
 
-  def test_logging_to_stdout(self):
-    '''
-      Call all log methods and review if the logs are
-      outputted to the standard output.
-    :return: None
-    '''
-    self.assertIsNotNone(self._logger)
-    self._logger.warn('Warn')
-    self._logger.error('Error')
-    self._logger.critical('Critical Error')
-    self._logger.debug('Debug')
-    #self._logger.exception('Exception') # Verifies an actual exception
-    self._logger.info('Information')
-    self._logger.trace('Trace')
+    def test_logging_to_stdout(self):
+        """
+        Call all log methods
+        :return:
+        """
+        self.assertIsNotNone(self._log, msg='Logger resource not found')
+        self._log.warn('Warn')
+        self._log.error('Error')
+        self._log.critical('Critical Error')
+        self._log.debug('Debug')
+        # self._logger.exception('Exception') # Verifies an actual exception
+        self._log.info('Information')
+        self._log.trace('Trace')
