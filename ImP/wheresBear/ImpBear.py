56169ccfefd1a3737323123fdc1a6c89773ca98f
andrewtran1995 <andrew.tran345@gmail.com>
2017-01-25 03:01:39 -0800

    Added PIL to requirements.txt; fixed formatting in ImpBear.py

--- ImP/wheresBear/ImpBear.py
+++ ImP/wheresBear/ImpBear.py
@@ -1,19 +1,19 @@
-from Imp.imageProcessing.imageProcessingInterface import ImageProcessor
-from Imp.wheresBear.wheresBearGenerator import generateDatabase
+from ImP.imageProcessing.imageProcessingInterface import ImageProcessor
+from ImP.wheresBear.wheresBearGenerator import generateDatabase
 import sys
 import glob
 
-sizeOfDB=sys.argv[1]
-markers=glob.glob('marker_*.png')
+sizeOfDB = sys.argv[1]
+markers = glob.glob('marker_*.png')
 
 generateDatabase(size=sizeOfDB,quarry_images=markers)
-images=glob.glob('Rot*_Pos*_Siz*_skew*')
-f=open('results.txt','w')
+images = glob.glob('Rot*_Pos*_Siz*_skew*')
+f = open('results.txt', 'w')
 for image in images:
     try:
-        imp=ImageProcessor(image)
+        imp = ImageProcessor(image)
         (corners, marker_ids) = imp._find_fiducial_markers()
-        f.write('found {0} at {1} in file {2}/n'.format(markers,corners,image))
+        f.write('found {0} at {1} in file {2}/n'.format(markers, corners, image))
     except:
         f.write('problem with {}/n'.format(image))
 f.close()
