7673d49c927e18abe2b5efab6b392657515e249e
andrewtran1995 <andrew.tran345@gmail.com>
2017-01-25 03:54:23 -0800

    Working script

--- ImP/wheresBear/ImpBear.py
+++ ImP/wheresBear/ImpBear.py
@@ -4,6 +4,11 @@ import sys
 import glob
 import os
 
+"""
+Can be called with:
+python3 -m ImP.wheresBear.ImpBear numImages
+"""
+
 sizeOfDB = int(sys.argv[1])
 wheres_bear_dir = os.path.dirname(__file__)
 print(wheres_bear_dir)
@@ -11,13 +16,19 @@ markers = glob.glob(os.path.join(wheres_bear_dir, 'marker_*.png'))
 backgrounds = [os.path.join(wheres_bear_dir, 'background.png')]
 
 generateDatabase(size=sizeOfDB, quarry_images=markers, backgrounds=backgrounds)
-images = glob.glob('Rot*_Pos*_Siz*_skew*')
+images = glob.glob(os.path.join(wheres_bear_dir, 'database', 'Rot*_Pos*_Siz*_skew*'))
+print("images is")
+print(images)
 f = open(os.path.join(wheres_bear_dir, 'results.txt'), 'w')
 for image in images:
     try:
         imp = ImageProcessor(image)
         (corners, marker_ids) = imp._find_fiducial_markers()
-        f.write('found {0} at {1} in file {2}/n'.format(markers, corners, image))
-    except:
+        if marker_ids is None:
+            f.write('found {0} at {1} in file {2}\n'.format(markers, corners, image))
+        else:
+            f.write('did not find {0} at {1} in file {2}\n'.format(markers, corners, image))
+    except Exception as ex:
+        print("Warning! Exception: {}".format(str(ex)))
         f.write('problem with {}/n'.format(image))
 f.close()
