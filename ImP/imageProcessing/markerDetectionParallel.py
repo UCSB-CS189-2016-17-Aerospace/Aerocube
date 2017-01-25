bf162e3b4810db434b168517cddbf732f3d541c6
andrewtran1995 <andrew.tran345@gmail.com>
2017-01-25 13:54:23 -0800

    Moved functions to class; updated unit-test

--- ImP/imageProcessing/markerDetectionParallel.py
+++ ImP/imageProcessing/markerDetectionParallel.py
@@ -7,47 +7,95 @@ from ImP.fiducialMarkerModule.fiducialMarker import FiducialMarker
 Need tests everywhere.
 * Test behavior for grayscale vs. non-grayscale input
 """
+class MarkerDetectionParallel:
+
+    # Set constants for parameter dict
+    adaptiveThreshWinSizeMin = 'adaptiveThreshWinSizeMin'
+    adaptiveThreshWinSizeMax = 'adaptiveThreshWinSizeMax'
+    adaptiveThreshWinSizeStep = 'adaptiveThreshWinSizeStep'
+    adaptiveThreshConstant = 'adaptiveThreshConstant'
+    minMarkerPerimeterRate = 'minMarkerPerimeterRate'
+    maxMarkerPerimeterRate = 'maxMarkerPerimeterRate'
+    polygonalApproxAccuracyRate = 'polygonalApproxAccuracyRate'
+    minCornerDistanceRate = 'minCornerDistanceRate'
+    minDistanceToBorder = 'minDistanceToBorder'
+    minMarkerDistanceRate = 'minMarkerDistanceRate'
+    doCornerRefinement = 'doCornerRefinement'
+    cornerRefinementWinSize = 'cornerRefinementWinSize'
+    cornerRefinementMaxIterations = 'cornerRefinementMaxIterations'
+    cornerRefinementMinAccuracy = 'cornerRefinementMinAccuracy'
+    markerBorderBits = 'markerBorderBits'
+    perspectiveRemovePixelPerCell = 'perspectiveRemovePixelPerCell'
+    perspectiveRemoveIgnoredMarginPerCell = 'perspectiveRemoveIgnoredMarginPerCell'
+    maxErroneousBitsInBorderRate = 'maxErroneousBitsInBorderRate'
+    minOtsuStdDev = 'minOtsuStdDev'
+    errorCorrectionRate = 'errorCorrectionRate'
+
+    # parameter dictionary/values
+    detectorParameters = {
+        adaptiveThreshWinSizeMin: 3,
+        adaptiveThreshWinSizeMax: 23,
+        adaptiveThreshWinSizeStep: 10,
+        adaptiveThreshConstant: 7,
+        minMarkerPerimeterRate: 0.03,
+        maxMarkerPerimeterRate: 4.,
+        polygonalApproxAccuracyRate: 0.03,
+        minCornerDistanceRate: 0.05,
+        minDistanceToBorder: 3,
+        minMarkerDistanceRate: 0.05,
+        doCornerRefinement: False,
+        cornerRefinementWinSize: 5,
+        cornerRefinementMaxIterations: 30,
+        cornerRefinementMinAccuracy: 0.1,
+        markerBorderBits: 1,
+        perspectiveRemovePixelPerCell: 4,
+        perspectiveRemoveIgnoredMarginPerCell: 0.13,
+        maxErroneousBitsInBorderRate: 0.35,
+        minOtsuStdDev: 5.0,
+        errorCorrectionRate: 0.6
+    }
+
+    @classmethod
+    def detect_markers_CUDA(cls, img, dictionary=FiducialMarker.get_dictionary()):
+        """
+
+        :param img:
+        :param dictionary:
+        :return:
+        """
+        # Raise exception if image is empty
+        if not img:
+            raise cls.CUDAFunctionException
+
+        # Convert to grayscale (if necessary)
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # STEP 1: Detect marker candidates
+        candidates, contours = cls.detect_candidates(gray_img)
+
+        # STEP 2
+
+        # STEP 3
+
+        # STEP 4
 
 
-def detect_markers_CUDA(img, dictionary=FiducialMarker.get_dictionary()):
-    """
+    @classmethod
+    def detect_candidates(cls, gray):
+        """
 
-    :param img:
-    :param dictionary:
-    :return:
-    """
-    # Raise exception if image is empty
-    if not img:
-        raise CUDAFunctionException
-
-    # Convert to grayscale (if necessary)
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # STEP 1: Detect marker candidates
-    candidates, contours = _detect_candidates(gray_img)
-
-    # STEP 2
-
-    # STEP 3
-
-    # STEP 4
+        :param gray: grayscale image to be analyzed
+        :return: marker candidates and marker contours
+        """
+        # Check if grayscale image is empty or is not actually a grayscale image
+        if gray is None:
+            raise cls.CUDAFunctionException
+        if gray.size is 0 or len(gray.shape) != 2:
+            raise cls.CUDAFunctionException
+        pass
 
 
-def _detect_candidates(gray):
-    """
-
-    :param gray: grayscale image to be analyzed
-    :return: marker candidates and marker contours
-    """
-    # Check if grayscale image is empty or is not actually a grayscale image
-    if not gray and len(gray.shape) != 2:
-        raise CUDAFunctionException
-
-
-    pass
-
-
-class CUDAFunctionException(Exception):
-    """
-    General exception for errors in the usage of the functions in this file.
-    """
+    class CUDAFunctionException(Exception):
+        """
+        General exception for errors in the usage of the functions in this file.
+        """
---
+++ ImP/imageProcessing/tests/testMarkerDetectionParallel.py
@@ -0,0 +1,28 @@
+import unittest
+import os
+import cv2
+from ImP.imageProcessing.markerDetectionParallel import *
+from ImP.imageProcessing.settings import ImageProcessingSettings
+
+
+class TestMarkerDetectionParallel(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls._IMAGE = cv2.imread(os.path.join(ImageProcessingSettings.get_test_files_path(), 'jetson_test1.jpg'))
+
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        self.image = self._IMAGE
+
+    def test_detect_candidates_raise_improper_image(self):
+        self.assertRaises(MarkerDetectionParallel.CUDAFunctionException, MarkerDetectionParallel.detect_candidates, self.image)
+        self.assertRaises(MarkerDetectionParallel.CUDAFunctionException, MarkerDetectionParallel.detect_candidates, None)
+
+if __name__ == '__main__':
+    unittest.main()
+
