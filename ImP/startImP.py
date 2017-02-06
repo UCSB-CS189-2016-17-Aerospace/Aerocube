8fae553ac1deaf7fbb02940c5449a6d8a450284a
andrewtran1995 <andrew.tran345@gmail.com>
2017-02-06 13:23:38 -0800

    Added test images

--- ImP/startImP.py
+++ ImP/startImP.py
@@ -7,17 +7,20 @@ exec(open("ImP/startImP.py").read())
 from ImP.imageProcessing.imageProcessingInterface import ImageProcessor
 from ImP.imageProcessing.cameraCalibration import CameraCalibration
 from ImP.imageProcessing.settings import ImageProcessingSettings
+import cv2
 from cv2 import aruco
 
-img_path = "ImP/imageProcessing/test_files/jetson_test1.jpg"
+img_path = "ImP/imageProcessing/test_files/GOPR0040.JPG"
+cal = CameraCalibration.PredefinedCalibration.GUS_GOPRO
 imp = ImageProcessor(img_path)
+corners, ids = imp._find_fiducial_markers()
 rvecs, tvecs = imp._find_pose()
-img = imp.draw_axis(CameraCalibration.PredefinedCalibration.ANDREW_IPHONE.CAMERA_MATRIX,
-                    CameraCalibration.PredefinedCalibration.ANDREW_IPHONE.DIST_COEFFS,
-                    imp.rodrigues_to_quaternion(rvecs[0]), tvecs, ImageProcessingSettings.get_marker_length())
-
-img_true = aruco.drawAxis(imp._img_mat,
-               CameraCalibration.PredefinedCalibration.ANDREW_IPHONE.CAMERA_MATRIX,
-               CameraCalibration.PredefinedCalibration.ANDREW_IPHONE.DIST_COEFFS,
-               rvecs, tvecs,
-               ImageProcessingSettings.get_marker_length())
+quaternions = [imp.rodrigues_to_quaternion(r) for r in rvecs]
+print(quaternions)
+print(tvecs)
+img = imp.draw_fiducial_markers(corners, ids)
+imp._img_mat = img
+img = imp.draw_axis(cal.CAMERA_MATRIX, cal.DIST_COEFFS, quaternions[0], tvecs[0])
+imp._img_mat = img
+img = imp.draw_axis(cal.CAMERA_MATRIX, cal.DIST_COEFFS, quaternions[1], tvecs[1])
+cv2.imwrite("ImP/output_files/GOPR0040_results_with_axes.JPG", img)
