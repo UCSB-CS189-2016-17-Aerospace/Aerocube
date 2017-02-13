783cd01d63c3aecf5a358bad043bf16bd9c26bb7
andrewtran1995 <andrew.tran345@gmail.com>
2017-02-13 00:23:26 -0800

    Adding CMakeLists.txt and fixing errors in markerDetectParGPU.cpp

--- ImP/imageProcessing/parallel/markerDetectParGPU.cpp
+++ ImP/imageProcessing/parallel/markerDetectParGPU.cpp
@@ -1,3 +1,12 @@
 #include "markerDetectParGPU.h"
-#include <opencv2/.h>
+#include <opencv2/core.hpp>
+#include <opencv2/cudawarping.hpp>
 
+void warpPerspectiveWrapper(cv::InputArray _src, cv::OutputArray _dst) {
+    int i = 0;
+    printf("ayy");
+}
+
+int main() {
+    return 1;
+}
