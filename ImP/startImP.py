c72185e793b3f4e7d2180109079d0a153f26ab71
andrewtran1995 <andrew.tran345@gmail.com>
2017-02-15 22:07:56 -0800

    Modified startImP.py and added capstone photoshoot files

--- ImP/startImP.py
+++ ImP/startImP.py
@@ -11,18 +11,9 @@ import cv2
 from cv2 import aruco
 
 img_path = "ImP/imageProcessing/test_files/GOPR0040.JPG"
-cal = CameraCalibration.PredefinedCalibration.GUS_GOPRO
-imp = ImageProcessor(img_path)
+imp = ImageProcessor(img_path, cal=CameraCalibration.PredefinedCalibration.GUS_GOPRO)
 corners, ids = imp._find_fiducial_markers()
 rvecs, tvecs = imp._find_pose()
 quaternions = [imp.rodrigues_to_quaternion(r) for r in rvecs]
 print(quaternions)
 print(tvecs)
-img = imp.draw_fiducial_markers(corners, ids)
-assert img.shape == imp._img_mat.shape
-imp._img_mat = img
-img1 = imp.draw_axis(cal.CAMERA_MATRIX, cal.DIST_COEFFS, quaternions[0], tvecs[0])
-cv2.imwrite("ImP/output_files/GOPR0040_results_with_axes_img1.JPG", img1)
-imp._img_mat = img1
-img2 = imp.draw_axis(cal.CAMERA_MATRIX, cal.DIST_COEFFS, quaternions[1], tvecs[1])
-cv2.imwrite("ImP/output_files/GOPR0040_results_with_axes_img2.JPG", img2)
