a2753ea3e53174b2175a7af4caaeb3e4a21ff0f8
andrewtran1995 <andrew.tran345@gmail.com>
2017-02-18 13:33:39 -0800

    Working Cython setup and pyx file (praise be to the ImP overlords)

--- ImP/imageProcessing/parallel/GpuWrapper.pyx
+++ ImP/imageProcessing/parallel/GpuWrapper.pyx
@@ -1,30 +1,42 @@
-import numpy as np
-cimport numpy as np
-from libcpp.string cimport string
-from libc.string import memcpy
+import numpy as np  # Import Python functions, attributes, submodules of numpy
+cimport numpy as np  # Import C functions, attributes, submodules of numpy
+from libcpp.string cimport string  # Import <string>
+from libc.string cimport memcpy  # Import <string.h>, or <cstring>
 
 """
 References:
 * http://makerwannabe.blogspot.com/2013/09/calling-opencv-functions-via-cython.html
 """
 
+# Define CV_8UC1 (8-bit, 1-channel matrix/image from core.hpp)
 cdef extern from 'opencv2/core/core.hpp':
     cdef int CV_8UC1
 
+# Define C++ class Mat from core.hpp
 cdef extern from 'opencv2/core/core.hpp' namespace 'cv':
     cdef cppclass Mat:
+        # Constructor
         Mat() except +
+        # Enum member
+        int AUTO_STEP
         void create(int, int, int)
-        void create(int, int, int, void*)
+        # void create(int, int, int, void*, size_t) except +
         void* data
 
 cdef void grayArr2cvMat(np.ndarray arr, Mat& out):
+    """
+    Convert a Python numpy object to a C++ Mat object
+    :param arr: Python numpy object of types np.ndarray; must be a 1 channel (grayscale) image
+    :param out: C++ Mat object for output
+    :return: void
+    """
     assert(arr.ndim == 2, "ASSERT::1 Channel Gray Image Only!")
-    cdef np.ndarray[np.uint8_t, ndim=2] np_buff = np.ascontiguousarray(arr, dtype=np.uint8_t)
+    # Note that we are using np.uint8_t (C++ object) for np_buff, but np.uint8 (Python object) in the Python method call
+    cdef np.ndarray[np.uint8_t, ndim=2] np_buff = np.ascontiguousarray(arr, dtype=np.uint8)
     cdef unsigned int* im_buff = <unsigned int*> np_buff.data
     cdef int rows = arr.shape[0]
     cdef int cols = arr.shape[1]
-    out.create(rows, cols, CV_8UC1, im_buff)
-    # out.create(rows, cols, CV_8UC1)
-    # memcpy(out.data, im_buff, rows*cols)
+    # out.create(<int> rows, <int> cols, CV_8UC1, <void *> im_buff, <size_t> out.AUTO_STEP)
+    out.create(rows, cols, CV_8UC1)
+    memcpy(out.data, im_buff, rows*cols)
 
--- ImP/imageProcessing/parallel/setupGpuWrapper.py
+++ ImP/imageProcessing/parallel/setupGpuWrapper.py
@@ -1,3 +1,4 @@
+import subprocess
 import os
 import numpy as np
 from distutils.core import setup, Extension
@@ -5,20 +6,31 @@ from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 
 """
+Run setup with the following command:
+```
+python setupGpuWrapper.py build_ext --inplace
+```
+
 References
 * http://cython.readthedocs.io/en/latest/src/reference/compilation.html
 """
 
 CUR_DIR = os.path.dirname(__file__)
+opencv_libs_str = subprocess.check_output("pkg-config --libs opencv".split()).decode()
+opencv_incs_str = subprocess.check_output("pkg-config --cflags opencv".split()).decode()
+opencv_libs = [str(lib) for lib in opencv_libs_str.strip().split()]
+opencv_incs = [str(inc) for inc in opencv_incs_str.strip().split()]
 
 extensions = [
     Extension('GpuWrapper',
               sources=[os.path.join(CUR_DIR, 'GpuWrapper.pyx')],
-              language='c++')
+              language='c++',
+              include_dirs=[np.get_include()] + opencv_incs,
+              extra_link_args=opencv_libs)
 ]
 
 setup(
     cmdclass={'build_ext': build_ext},
     name="GpuWrapper",
-    ext_modules=cythonize(extensions, include_path=[np.get_include()])
+    ext_modules=cythonize(extensions)
 )
