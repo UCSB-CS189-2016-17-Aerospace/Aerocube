7f2a7403641ce1738080558e4373bb0a25a23b32
Gustavo Cornejo <gcornejo85@gmail.com>
2017-02-22 14:25:26 -0800

    added try statement to avoid aborting if cuda not found

--- shellScripts/curlCommand.sh
+++ shellScripts/curlCommand.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-IMG1=/home/$USER/GitHub/Aerocube/ImP/imageProcessing/test_files/jetson_test1.jpg
+IMG1=/Users/gustavo/Documents/GitHub/Aerocube/ImP/imageProcessing/test_files/jetson_test1.jpg
 echo "Curling $IMG1"
 curl -F photo=@${IMG1} https://localhost:3000/api/uploadImage -k
 # echo "/home/ubuntu/GitHub/Aerocube/flaskServer/static/img/epicEarthMoon.png"
