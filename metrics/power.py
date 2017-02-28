05a7ff671830095b0df3f0f4ae94fc58f900d15d
Angel Ortega <an22ortega@gmail.com>
2017-02-28 01:39:57 -0800

    power consumption metrics

---
+++ metrics/power.py
@@ -0,0 +1,24 @@
+import subprocess
+import shlex
+
+class Power():
+  
+  def __init__(self):
+    self._power_now = "awk '{print $1*10^-6 \" W\"}' /sys/class/power_supply/BAT0/power_now"
+  
+  def power_now(self):
+    """
+    Power consumption returned in microwatts (µW)
+    Look in `/sys/sys/power_supply/BAT0/power_now`
+    cat /sys/class/power_supply/BAT0/power_now
+    awk '{print $1*10^-6 " W"}' /sys/class/power_supply/BAT0/power_now
+    """
+    proc = subprocess.Popen(self._power_now, stdout=PIPE) # Might need to set shell=True
+    out, err = proc.communicate()
+    return out
+    # may need to try proc.poll
+
+  def power_now2(self):
+    args = shlex.split(self._power_now)
+    out = subprocess.check_output(args)
+    return out
---
+++ metrics/power_monitor.py
@@ -0,0 +1,11 @@
+class PowerMonitor():
+
+  def __init__(self):
+    self
+
+  def enable(self):
+    self
+
+  def disable(self):
+    self
+   
