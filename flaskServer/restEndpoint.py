6033883f66dd3df456918a7a19448dd74bc52745
athielk <alexthielk@gmail.com>
2017-03-04 12:10:08 -0800

    AT Authentication by email login

--- flaskServer/restEndpoint.py
+++ flaskServer/restEndpoint.py
@@ -171,7 +171,7 @@ class FireEndpoint:
         self.storage = self.firebase.storage()
         auth = self.firebase.auth()
         #user=auth.sign_in_with_custom_token(self.token)
-        #user = auth.sign_in_with_email_and_password('yourfirenation@gmail.com', 'yourfirenation')
+        user = auth.sign_in_with_email_and_password('yourfirenation@gmail.com', 'yourfirenation')
         self.my_stream = self.db.child("uploads").stream(self.stream_handler)
 
     def create_new_job(self):
